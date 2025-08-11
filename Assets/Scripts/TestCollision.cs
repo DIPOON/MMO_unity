7a5ccd564efe5164f8846c6eb4aac07d5c915268
DiPoon <6129876j@naver.com>
2025-08-11 23:46:56 +0900

    Raycasting#2

--- Assets/Scripts/TestCollision.cs
+++ Assets/Scripts/TestCollision.cs
@@ -29,6 +29,32 @@ public class TestCollision : MonoBehaviour
     void Update()
     {
         // Debug.Log(Input.mousePosition); // Screen 픽셀
-        Debug.Log(Camera.main.ScreenToViewportPoint(Input.mousePosition)); // Viewport 화면 비율
+        // Debug.Log(Camera.main.ScreenToViewportPoint(Input.mousePosition)); // Viewport 화면 비율
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            
+            Debug.DrawRay(Camera.main.transform.position, ray.direction * 100.0f, Color.red, 1.0f);
+            RaycastHit hit;
+            
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                Debug.Log($"Raycast Camera @ {hit.collider.gameObject.name}");
+            }
+            
+            // Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y,
+            //     Camera.main.nearClipPlane));
+            // Vector3 dir = mousePos - Camera.main.transform.position;
+            // dir = dir.normalized;
+            //
+            // Debug.DrawRay(Camera.main.transform.position, dir * 100.0f, Color.red, 1.0f);
+            //
+            // RaycastHit hit;
+            // if (Physics.Raycast(Camera.main.transform.position, dir, out hit, 100.0f))
+            // {
+            //     Debug.Log($"Raycast Camera @ {hit.collider.gameObject.name}");
+            // }
+        }
     }
 }
