798c889c1916eb6bbc90ef133d2d8945ba8d454d
DiPoon <6129876j@naver.com>
2025-08-24 17:13:26 +0900

    에니메이션 기초 나머지 + 블렌딩 초기

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -35,6 +35,16 @@ public class PlayerController : MonoBehaviour
                 transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 10 * Time.deltaTime);
             }
         }
+        
+        Animator animator = GetComponent<Animator>();
+        if (_moveToDest)
+        {
+            animator.Play("RUN");    
+        }
+        else
+        {
+            animator.Play("WAIT");            
+        }
     }
 
     void OnKeyboard()
@@ -67,9 +77,6 @@ public class PlayerController : MonoBehaviour
 
     void OnMouseClicked(Define.MouseEvent evt)
     {
-        if (evt != Define.MouseEvent.Click)
-            return;
-        
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         Debug.DrawRay(Camera.main.transform.position, ray.direction * 100.0f, Color.red, 1.0f);
         LayerMask mask = LayerMask.GetMask("Wall");
