c41337af5e1f8e44c5655eba6f067e388f416d71
DiPoon <6129876j@naver.com>
2026-02-01 23:12:15 +0900

    UI 자동화 2

--- Assets/Scripts/UI/UI_Button.cs
+++ Assets/Scripts/UI/UI_Button.cs
@@ -4,10 +4,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UI_Button : MonoBehaviour
+public class UI_Button : UI_Base
 {
-    Dictionary<Type, UnityEngine.Object[]> _object = new Dictionary<Type, UnityEngine.Object[]>();
-    
     enum Buttons
     {
         PointButton
@@ -19,22 +17,18 @@ public class UI_Button : MonoBehaviour
         ScoreText
     }
 
+    enum GameObjects
+    {
+        TestObject,
+    }
+
     private void Start()
     {
         Bind<Button>(typeof(Buttons));
         Bind<Text>(typeof(Texts));
-    }
+        Bind<GameObject>(typeof(GameObjects));
 
-    void Bind<T>(Type type) where T : UnityEngine.Object
-    {
-        string[] names = Enum.GetNames(type);
-        UnityEngine.Object[] objects = new UnityEngine.Object[names.Length];
-        _object.Add(typeof(T), objects);
-
-        for (int i = 0; i < names.Length; i++)
-        {
-            objects[i] = Util.FindChild<T>(gameObject, names[i], true);
-        }
+        GetText((int)Texts.ScoreText).text = "Bind Test";
     }
 
     private int _score = 0;
